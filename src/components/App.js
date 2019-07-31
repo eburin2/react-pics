2b95121c5891a8153caaaa794ca3bcaf779a2d3c
Elliot Burin <eburin2@gmail.com>
2019-07-31 13:26:01 -0700

    add css grid to display images

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import unsplash from '../api/unsplash';
 import SearchBar from './SearchBar';
+import ImageList from './ImageList';
 
 class App extends React.Component {
-  state = { images: [] }
+  state = { images: [] };
   onSearchSubmit = async (term) => {
     const response = await unsplash.get('/search/photos', {
       params: { query: term }
@@ -16,7 +17,7 @@ class App extends React.Component {
     return (
       <div className="ui container" style={{marginTop: '10px'}}>
         <SearchBar onSubmit={this.onSearchSubmit}/>
-        Found: {this.state.images.length}
+        <ImageList images={this.state.images} />
       </div>
     );
   }
